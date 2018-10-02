c5c1ec6e812a9fa2a958005c5597b869ca665d32
robertlemon <robertlemon@outlook.com>
2018-10-02 13:39:29 +0200

    Initial commit

---
+++ src/Robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'lg@april.biz'
+	},
+	{
+		id: 2,
+		name: 'Erwin Howell',
+		username: 'Ewwe',
+		email: 'ew@may.biz'
+	},
+	{
+		id: 3,
+		name: 'Clementine Bauch',
+		username: 'Bauchen',
+		email: 'bauchen@clementine.com'
+	},
+	{
+		id: 4,
+		name: 'Pat Lebsack',
+		username: 'Lebsack',
+		email: 'lebsack@pat.com'
+	},
+	{
+		id: 5,
+		name: 'Chelsey Dietrich',
+		username: 'Dietrich',
+		email: 'chelsey@dietrich.org'
+	}
+]
